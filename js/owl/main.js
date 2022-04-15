8ba3c0bdc1e30078c58049ed05e2f39c326d4f0d
vinigiu <viniciusgiuseppe1992@gmail.com>
2022-04-15 18:27:04 -0300

    Arquivos site oBotFlix - Clone da Netflix

---
+++ js/owl/main.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+    loop:false, //Se quiser que seja infinito o loop: true//
+    margin:10, //Espaço entre os itens do carrosel//
+    nav:false, //Se quiser que coloque as <- -> para navegação no carrosel//
+    responsive:{ //Trata da responsividade de acordo com o tamanho da tela, quantos itens ele exibe do carrosel//
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
